daa1beebaea04a088e0153e4476aaf805aaf6bda
Nathan Samson <nathansamson@gmail.com>
2009-12-30 21:02:12 +0100

    Add combobox for resolution.

---
+++ widgets/combobox.h
@@ -0,0 +1,55 @@
+#ifndef INC_COMBOBOX_H
+#define INC_COMBOBOX_H
+
+/** \file
+ * File for the comboboxwidget.
+ *
+ * @author Nathan Samson
+*/
+
+#include <vector>
+
+#include "widgets/hbox.h"
+#include "widgets/button.h"
+#include "widgets/label.h"
+
+namespace Zabbr {
+	class ComboBox: public HBox {
+	public:
+		ComboBox(SDLWindow*);
+		
+		void addOption(std::string);
+		std::string getSelectedOption();
+	
+	private:
+		void setOption(int);
+		void onWheelButtonClicked(Button*);
+	
+		/**
+		 * A list of options.
+		*/
+		std::vector<std::string> fOptions;
+		
+		/**
+		 * The index of the selected options, a negative number is nothing selected.
+		*/
+		int fSelectedOption;
+	
+		/**
+		 * The label used to print the active option.
+		*/
+		Label* fLabel;
+		
+		/**
+		 * The up button.
+		*/
+		Button* fUpButton;
+		
+		/**
+		 * The down button.
+		*/
+		Button* fDownButton;
+	};
+}
+
+#endif // INC_COMBOBOX_H
